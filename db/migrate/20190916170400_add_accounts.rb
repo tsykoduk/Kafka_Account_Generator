1d4d6b281c381ccb2f891b1d663eecc8f682a42a
Greg Nokes <tsykoduk@gmail.com>
2019-09-16 10:45:58 -0700

    has it been this long since I wrote a migration

--- db/migrate/20190916170400_add_accounts.rb
+++ db/migrate/20190916170400_add_accounts.rb
@@ -1,21 +1,21 @@
 class AddAccount < ActiveRecord::Migration[6.0]
   def change
     
-    create_table "account", do |t|
-      t.string   "billingpostalcode", limit: 20
-      t.string   "sfid",              limit: 18
-      t.string   "_c5_source",        limit: 18
+    create_table "account" do |t|
+      t.string   "billingpostalcode" limit: 20
+      t.string   "sfid"              limit: 18
+      t.string   "_c5_source"        limit: 18
       t.datetime "lastmodifieddate"
       t.string   "website"
       t.string   "billingstreet"
       t.string   "name"
-      t.string   "tickersymbol",      limit: 20
-      t.string   "billingcity",       limit: 40
+      t.string   "tickersymbol"      limit: 20
+      t.string   "billingcity"       limit: 40
       t.boolean  "isdeleted"
-      t.string   "phone",             limit: 40
-      t.string   "billingstate",      limit: 80
-      t.string   "billingcountry",    limit: 80
-      t.string   "external_id__c",    limit: 80
+      t.string   "phone"             limit: 40
+      t.string   "billingstate"      limit: 80
+      t.string   "billingcountry"    limit: 80
+      t.string   "external_id__c"    limit: 80
       t.datetime "created_at"
       t.datetime "updated_at"
     end
